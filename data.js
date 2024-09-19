8b7400aaa87059695dac5b0313013a1dcf136eff
jfqvinueza <jfq.vinueza@yavirac.edu.ec>
2024-09-19 17:34:55 -0500

    first commit

--- data.js
+++ data.js
@@ -8,14 +8,16 @@ const categorias = [
     { id: 'portafolios', nombre: 'Portafolios' },
     { id: 'canguros', nombre: 'Canguros' },
     { id: 'militares', nombre: 'Militares' },
-    { id: 'estuches', nombre: 'Estuches Instrumentos Musicales' }
+    { id: 'estuches', nombre: 'Estuches Instrumentos Musicales' },
+    { id: 'jovenes', nombre: 'jovenes' }
+
 ];
 
 // Datos de productos asociados a cada categoría
 const productos = {
     campis: [
-        { nombre: "Campi 1", imagen: "https://example.com/campi1.jpg" },
-        { nombre: "Campi 2", imagen: "https://example.com/campi2.jpg" }
+        { nombre: "Campi 1", imagen: "https://firebasestorage.googleapis.com/v0/b/psyched-service-419202.appspot.com/o/alexMochilas%2FWhatsApp%20Image%202024-09-19%20at%2016.15.40%20(1).jpeg?alt=media&token=f13086f7-0a20-4aa9-9651-2c9ce7fa318f" },
+        { nombre: "Campi 2", imagen: "https://firebasestorage.googleapis.com/v0/b/psyched-service-419202.appspot.com/o/alexMochilas%2FWhatsApp%20Image%202024-09-19%20at%2016.15.40.jpeg?alt=media&token=6947f4c0-f1f4-406d-a06a-7157d5c5b805" }
     ],
     ninios: [
         { nombre: "Mochila Niños 1", imagen: "https://example.com/ninios1.jpg" },
@@ -29,7 +31,14 @@ const productos = {
     zapaterias: [],
     portaTernos: [],
     portafolios: [],
-    canguros: [],
+    canguros: [
+        { nombre: "Canguro 1", imagen: "https://firebasestorage.googleapis.com/v0/b/psyched-service-419202.appspot.com/o/alexMochilas%2FWhatsApp%20Image%202024-09-19%20at%2016.16.24.jpeg?alt=media&token=d629f26c-747c-4e21-b68c-59711a6212e6" },
+        { nombre: "Canguro 2", imagen: "https://example.com/cartuchera2.jpg" }
+    ],
     militares: [],
-    estuches: []
+    estuches: [],
+    jovenes:[
+        { nombre: "jovenes uno", imagen: "https://firebasestorage.googleapis.com/v0/b/psyched-service-419202.appspot.com/o/alexMochilas%2FWhatsApp%20Image%202024-09-19%20at%2016.15.41%20(1).jpeg?alt=media&token=108f871f-ef3b-46a5-8fb6-34d1ef74e223" }
+
+    ]
 };
